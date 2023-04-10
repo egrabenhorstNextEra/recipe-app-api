9d571bf013acc659ed576861c5f2b319519015cd
egrabenhorst <egrabenhorst@predictintel.com>
2023-04-10 12:05:58 -0400

    initial commit for recipe api application

---
+++ app/app/calc.py
@@ -0,0 +1,19 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """
+    :param x:
+    :param y:
+    :return: x + y
+    """
+    return x+y
+
+def subtract(x,y):
+    """
+    :param x:
+    :param y:
+    :return: y-x
+    """
+    return y-x
